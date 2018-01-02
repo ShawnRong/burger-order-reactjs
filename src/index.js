1c0ff8892bd63ae9e6549582b958cc3255bdd183
ShawnRong <=>
2018-01-02 17:19:08 +0800

    Transform redux-thunk to saga

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
+import createSagaMiddleware from 'redux-saga'
 import App from './App'
 import burgerBuilderReducer from './store/reducers/burgerBuilder'
 import orderReducer from './store/reducers/order'
 import authReducer from './store/reducers/auth'
+import { watchAuth } from './store/sagas'
 import './css/index.css'
 
 const rootReducer = combineReducers({
@@ -15,12 +17,17 @@ const rootReducer = combineReducers({
   order: orderReducer,
   auth: authReducer
 })
+
+const sagaMidddleware = createSagaMiddleware()
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   rootReducer,
-  composeEnhancers(applyMiddleware(thunk))
+  composeEnhancers(applyMiddleware(thunk, sagaMidddleware))
 )
+
+sagaMidddleware.run(watchAuth)
 
 const app = (
   <Provider store={store}>
